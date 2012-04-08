da012d3d4f43ccb4d3a999a1b94107d59b2c1b99
Martin Walsh <github@mwalsh.org>
2012-04-08 01:22:25 -0500

    code cleanup (con't)

--- nagios/examples/factory_example.py
+++ nagios/examples/factory_example.py
@@ -5,9 +5,9 @@ from plugin import NagiosPluginFactory
 
 
 if __name__ == '__main__':
-    def check():
+    def some_function():
         return [(10, 'Value %(value)s is %(status)s')]
     
-    NagiosPluginFactory('CustomCheck', check, network=False).check()
+    NagiosPluginFactory('CustomCheck', some_function, network=False).check()
 
 
